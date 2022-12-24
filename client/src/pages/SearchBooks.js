5f88e1c4e520fcca254624726a1481204c802a97
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-24 11:46:46 -0600

    adding back deleted starter code for localStorage and initial reworking of SearchBooks.js page

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+// import { QUERY_ME } from '../utils/queries';
+import { SAVE_BOOK } from '../utils/mutations';
 
-import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+// import Auth from '../utils/auth';
+// import { saveBook, searchGoogleBooks } from '../utils/API';
+// import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
-  useEffect(() => {
-    return () => saveBookIds(savedBookIds);
-  });
+//   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
+//   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+//   useEffect(() => {
+//     return () => saveBookIds(savedBookIds);
+//   });
+  
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -56,7 +59,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    const [saveBook, { error, data }] = useMutation(SAVE_BOOK);
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
