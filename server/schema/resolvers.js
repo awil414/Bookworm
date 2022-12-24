e14c75e09c9f7c17efba755bdfbe9d943650d021
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-23 22:37:51 -0600

    adding comments to resolvers file

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -15,28 +15,32 @@ const resolvers = {
 
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
+       // Create the user 
       const user = await User.create({ username, email, password });
+      // Immediately assign a JSON WebToken and log the user in 
       const token = signToken(user);
+      // Return an `Auth` object that consists of the signed token and user's info
       return { token, user };
     },
     login: async (parent, { email, password }) => {
+      // Look up user by the provided email
       const user = await User.findOne({ email });
 
       if (!user) {
         throw new AuthenticationError('No user found with this email address');
       }
-
+      // If there is a found user, execute the `isCorrectpassword` instance method and check if correct password provided
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
-
+      // If email and password are correct, sign user into the application with a JWT
       const token = signToken(user);
-
+      // Return an `Auth` object that consists of the signed token and user's info
       return { token, user };
     },
-    // IS THIS RIGHT??????????????
+   
     saveBook: async (parent, { book }, context) => {
       if (context.user) {
         const updateUser = await User.findByIdAndUpdate(
